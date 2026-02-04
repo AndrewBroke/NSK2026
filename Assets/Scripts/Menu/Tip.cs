96bb7eda7a9e58573b707c03e5e2309c94bcd30c
AndrewBroke <kuzukov-work@yandex.ru>
2026-02-04 20:14:51 +0700

    Tip update

--- Assets/Scripts/Menu/Tip.cs
+++ Assets/Scripts/Menu/Tip.cs
@@ -1,13 +1,37 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Tip : MonoBehaviour
 {
-    [SerializeField] Text tipTextUI;
+    Text tipTextUI;
+    string tipTextToDisplay;
 
+    private void Start()
+    {
+        tipTextUI = GetComponent<Text>();
+    }
     public void SetTip(string tipText)
     {
-        tipTextUI.text = tipText;
-        
+        print(123);
+        tipTextToDisplay = tipText;
+        StartCoroutine("TipFadeIn");
+    }
+
+    public void RemoveTip()
+    {
+        StopAllCoroutines();
+        tipTextUI.text = "";
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    IEnumerator TipFadeIn()
+    {
+        yield return new WaitForSeconds(2);
+        tipTextUI.text = tipTextToDisplay;
     }
 }
